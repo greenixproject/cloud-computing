e266f65f404f2e3b72bc8bf5a603b6da3d3b571c
Gilang Swandaru <49299884+gilanghyro@users.noreply.github.com>
2023-05-28 18:34:31 +0800

    Update API-TensorFlow.py

--- API-TensorFlow.py
+++ API-TensorFlow.py
@@ -1,28 +1,21 @@
-from flask import Flask, request, jsonify
 import tensorflow as tf
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# Load the trained model
-model = tf.keras.models.load_model('path_to_model')
-
-# Define the API endpoint
-@app.route('/api/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    # Get the input data from the request
-    data = request.json
+    # Ambil data input dari permintaan POST
+    data = request.get_json()
 
-    # Preprocess the input data if necessary
-    # ...
+    # Lakukan prediksi menggunakan model TensorFlow
+    # Ganti dengan logika prediksi yang sesuai dengan aplikasi Anda
+    prediction = model.predict(data)
 
-    # Perform inference using the loaded model
-    result = model.predict(data)
-
-    # Postprocess the result if necessary
-    # ...
-
-    # Return the predicted result
-    return jsonify({'result': result.tolist()})
+    # Mengembalikan hasil prediksi sebagai respons JSON
+    return {'prediction': prediction}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Load model TensorFlow
+    model = tf.keras.models.load_model('model.h5')
+    app.run(host='0.0.0.0', port=8080)
